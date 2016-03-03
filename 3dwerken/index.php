cb8d07d93776b911a41646ca6867db00a781b741
levien4 <levienvdw@gmail.com>
2016-03-03 14:47:09 +0100

    informatie 3d Werken toegevoegd

--- 3dwerken/index.php
+++ 3dwerken/index.php
@@ -29,7 +29,12 @@
 			<h3>Omschrijving:</h3>
 			<div id="info">
 				<a href="http://www.humancreature.nl/3dwerken/keramiek">Keramiek:</a><br>
-				Produkt van gebakken en eventueel geglazuurde klei
+				Produkt van gebakken en eventueel geglazuurde klei<br>
+				Grondstof = vette klei – veel plastisch materiaal (goed kneedbaar) , magere klei –  fijne chamotte,  grove chamotte - wit-, bruin-, rood-, zwartbakkend, gres
+Biscuit = ongeglazuur, slechts eenmaal gebakken produkt
+Sommige werkstukken zijn biscuitgebrand en met metaaloxydes gekleurd - dus niet met glazuur afgewerkt.
+De koppen, vogels en zeehonden zijn met uitgerolde fijne of grove chamotte kleidelen opgebouwd al of niet om een papieren kern. Metaaloxydes ( koper-, mangaan-, ijzer-, chroom-, kobal-, tinoxydes) en glazuren bepalen de kleur en oppervlakteaccenten.
+De boekrol heeft als beginvorm een kleiplaat met inscripties; vervolgens gegoten in gips en afdrukt met chamotteklei – deze tekstplaat is rondgezet, gedroogd, biscuit gebakken 800 graden C, geglazuurd en gebakken op 1080 graden C.
 			</div>
 		</center>
 	</div>
