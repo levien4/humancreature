d5addecee588a11eaf08f4c88bf3a9b12f233d08
levien4 <levienvdw@gmail.com>
2016-03-04 16:12:15 +0100

    biografie aanpassingen

--- Biografie/index.php
+++ Biografie/index.php
@@ -27,63 +27,62 @@
 		<center>
 			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
 			<div id="biografie">
-			opleiding:<br>
-<ul>1952-1956 MULO – Utrecht, Assen, Gorinchem
-<li>1956-1961 Pedagogische Academie “Juliana van Stolberg” Gorinchem</li>
-<li>Staatsdiploma L.O.-Handvaardigheid 1970</li>
-<li>Voortgezette Studie Handvaardigheid 1964-1972: Rotterdam, Den Haag, Hengelo, Groningen</li>
-<li>Staatsdiploma Handvaardigheid A/B, Amersfoort 1982</li>
-<li>Post HBO – tekenen als observatiemethodiek – psycho-chromoanalyse – diagnostiek – pictologie van de kindertekening, Rijks Hogeschool Groningen, 1995</li>
-<li>Post HBO - Interpreteren van Kindertekeningen – SON-cursus 1998, Utrecht</li>
+				opleiding:<br>
+				<li>1952-1956 MULO – Utrecht, Assen, Gorinchem</li>
+				<li>1956-1961 Pedagogische Academie “Juliana van Stolberg” Gorinchem</li>
+				<li>Staatsdiploma L.O.-Handvaardigheid 1970</li>
+				<li>Voortgezette Studie Handvaardigheid 1964-1972: Rotterdam, Den Haag, Hengelo, Groningen</li>
+				<li>Staatsdiploma Handvaardigheid A/B, Amersfoort 1982</li>
+				<li>Post HBO – tekenen als observatiemethodiek – psycho-chromoanalyse – diagnostiek – pictologie van de kindertekening, Rijks Hogeschool Groningen, 1995</li>
+				<li>Post HBO - Interpreteren van Kindertekeningen – SON-cursus 1998, Utrecht</li>
 
-tentoonstellingen:
-<li>keramiek				 		- 	Nieuw Lekkerland</li>
-<li>keramiek				 		- 	Hardenberg</li>
-<li>fotografie Eiertikken	 		 	- 	Gemeentehuis Ommen</li>
-<li>fotografie, keramiek en druktechn.	 		- 	“Het Baken” Ommen, Steenwijk</li>
-<li>fotografie				 	- 	Kleine Kerk Steenwijk</li>
-<li>druktechnieken & keramiek		 		- 	GH Zwolle (diverse locaties)</li>
-<li>keramiek, collage-/assemblagetechn	 		- 	Galerie “Kwintsheul” 2009.</li>
-<li>keramiek en druktechnieken		 		- 	Wouwse Plantage</li>
-<li>druktechnieken:Groot Nieuwsradio	 		- 	Venendaal 2009</li>
-<li>overzichtstentoonstelling		 		- 	Meppel Kruiskerk 2009/’10</li>
-<li>expositie druktechnieken		 		- 	Zwartsluis GKv febr-mei ’10</li>
-<li>diverse exposities keramiek/druktechn			-	Van der Valk Hotel-Restaurant-keten 2011&2012</li>
-<li>keramiek				 		-	Bergen a/z 18/20 mei 2013 combi-expo Hotel Meijer</li>
+				tentoonstellingen:
+				<li>keramiek				 		- 	Nieuw Lekkerland</li>
+				<li>keramiek				 		- 	Hardenberg</li>
+				<li>fotografie Eiertikken	 		 	- 	Gemeentehuis Ommen</li>
+				<li>fotografie, keramiek en druktechn.	 		- 	“Het Baken” Ommen, Steenwijk</li>
+				<li>fotografie				 	- 	Kleine Kerk Steenwijk</li>
+				<li>druktechnieken & keramiek		 		- 	GH Zwolle (diverse locaties)</li>
+				<li>keramiek, collage-/assemblagetechn	 		- 	Galerie “Kwintsheul” 2009.</li>
+				<li>keramiek en druktechnieken		 		- 	Wouwse Plantage</li>
+				<li>druktechnieken:Groot Nieuwsradio	 		- 	Venendaal 2009</li>
+				<li>overzichtstentoonstelling		 		- 	Meppel Kruiskerk 2009/’10</li>
+				<li>expositie druktechnieken		 		- 	Zwartsluis GKv febr-mei ’10</li>
+				<li>diverse exposities keramiek/druktechn			-	Van der Valk Hotel-Restaurant-keten 2011&2012</li>
+				<li>keramiek				 		-	Bergen a/z 18/20 mei 2013 combi-expo Hotel Meijer</li>
 
-werkzaamheden:<br>
-<li>Koninklijke Marine				 -	1961-1963 Verbindingsschool A’ dam, SDO Hollandse Rading</li>
-<li>Geref. Lager- & Basisonderwijs 			- 	1961-1980 Kinderdijk, Rijswijk, Ommen</li>
-<li>MULO/MAVO					-	1968-1972 Hardenberg (part-time)</li>
-<li>GH-Zwolle					- 	1973-1980 (part-time) Beeldende Vorming</li>
-<li>GH-Zwolle					- 	1980-1992 afd. hoofd  Propedeuse</li>
-<li>GH-Zwolle					- 	1980-2000 full time docent</li>
+				werkzaamheden:<br>
+				<li>Koninklijke Marine				 -	1961-1963 Verbindingsschool A’ dam, SDO Hollandse Rading</li>
+				<li>Geref. Lager- & Basisonderwijs 			- 	1961-1980 Kinderdijk, Rijswijk, Ommen</li>
+				<li>MULO/MAVO					-	1968-1972 Hardenberg (part-time)</li>
+				<li>GH-Zwolle					- 	1973-1980 (part-time) Beeldende Vorming</li>
+				<li>GH-Zwolle					- 	1980-1992 afd. hoofd  Propedeuse</li>
+				<li>GH-Zwolle					- 	1980-2000 full time docent</li>
 
-vrijwilligers werk:<br>
-<li>Reisleider GRV					-	1987 Joegoslavië/Venetië</li>
-<li>Stichting Kunst & Cultuur Kleine Kerk Steenwijk 	-	voorzitter St. K&CK Steenwijk 1998-2009</li>
-<li>Weeshuis:Maseru Lesotho				-	2004/2005 Creatief werken</li>
-<li>Juvenile Training Centre 				-	2009 gastcollege trainers/pupils YfC – Maseru, Lesotho</li>
-<li>Disciple Training School  				-	gastcolleges 2008/2009 YwaM (Maseru)</li>
-<li>Youth for Christ, Maseru				-	Shape Day en townshipwork 2008 leiders & jeugdtraining o.l.v. Gert v d Bos e.a.</li>
+				vrijwilligers werk:<br>
+				<li>Reisleider GRV					-	1987 Joegoslavië/Venetië</li>
+				<li>Stichting Kunst & Cultuur Kleine Kerk Steenwijk 	-	voorzitter St. K&CK Steenwijk 1998-2009</li>
+				<li>Weeshuis:Maseru Lesotho				-	2004/2005 Creatief werken</li>
+				<li>Juvenile Training Centre 				-	2009 gastcollege trainers/pupils YfC – Maseru, Lesotho</li>
+				<li>Disciple Training School  				-	gastcolleges 2008/2009 YwaM (Maseru)</li>
+				<li>Youth for Christ, Maseru				-	Shape Day en townshipwork 2008 leiders & jeugdtraining o.l.v. Gert v d Bos e.a.</li>
+				<li>Workshop druktechnieken 				- 	Meppel 2009 GKv gemeente aktiviteit</li>
+				<li>Workshop druktechnieken				-	Zwartsluis 2010 GKv gemeente aktiviteit</li>
+				<li>Workshop druktechnieken 				- 	Ladybrand 2010 Chr. Jeugdcentrum</li>
+				<li>Workshop druktechnieken 				- 	Ladybrand 2011 met jongeren uit de buurt</li>
+				<li>Workshop druktechnieken  St. Presen Steenwijkerland 	- 	Steenwijk 2012 Nijenstede Bejaarden</li>
+				<li>Workshop druktechnieken 	idem			- 	Steenwijk 2012 groep begeleidwonen</li>
+				<li>Workshop druktechnieken 	idem			- 	Steenwijk 2012  2 x groep dagopvang</li>
+				<li>Evangelisatie & Recreatie 	2013, 2015		-	programma creatief werk op camping Eikenhorst in Paasloo</li>
 
-Workshop druktechnieken 				- 	Meppel 2009 GKv gemeente aktiviteit
-Workshop druktechnieken				-	Zwartsluis 2010 GKv gemeente aktiviteit
-Workshop druktechnieken 				- 	Ladybrand 2010 Chr. Jeugdcentrum
-Workshop druktechnieken 				- 	Ladybrand 2011 met jongeren uit de buurt
-Workshop druktechnieken  St. Presen Steenwijkerland 	- 	Steenwijk 2012 Nijenstede Bejaarden
-Workshop druktechnieken 	idem			- 	Steenwijk 2012 groep begeleidwonen
-Workshop druktechnieken 	idem			- 	Steenwijk 2012  2 x groep dagopvang
-Evangelisatie & Recreatie 	2013, 2015		-	programma creatief werk op camping Eikenhorst in Paasloo
-
-divers:
-Illustratie omslag: Academisch Proefschrft “DE TWEEDE TROMPET”- prof. dr P.H.R. van Houwelingen, uitg. J.H. Kok Kampen, 1998
-Illustraties in “Jesus stel homself bekend” Rob van Houwelingen  en Aldert L. Visser 2012
-Artikelen maandblad  Aan de Hand - Creativiteit en Expressie
-BEELDENDE VORMING en WELZIJN – uitgave GHBO – Zwolle, 1980
-Lid Cie. Beeldende Kunst Steenwijkerland (twee periodes) en mede auteur Beelden in Beeld 2 & 3 uitgave Kunst en Cultuur, Steenwijkerland
-Bekijk je Rijk – artikelen over kunst en cultuur in kerkblad en GHBO-uitgaven
-</div>
+				divers:
+				<li>Illustratie omslag: Academisch Proefschrft “DE TWEEDE TROMPET”- prof. dr P.H.R. van Houwelingen, uitg. J.H. Kok Kampen, 1998</li>
+				<li>Illustraties in “Jesus stel homself bekend” Rob van Houwelingen  en Aldert L. Visser 2012</li>
+				<li>Artikelen maandblad  Aan de Hand - Creativiteit en Expressie</li>
+				<li>BEELDENDE VORMING en WELZIJN – uitgave GHBO – Zwolle, 1980</li>
+				<li>Lid Cie. Beeldende Kunst Steenwijkerland (twee periodes) en mede auteur Beelden in Beeld 2 & 3 uitgave Kunst en Cultuur, Steenwijkerland</li>
+				<li>Bekijk je Rijk – artikelen over kunst en cultuur in kerkblad en GHBO-uitgaven</li>
+				</div>
 		</center>
 	</div>
 </div>
