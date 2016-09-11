aa78348603220641792f3f63b7bc50d968b8e5ca
levien4 <levienvdw@gmail.com>
2016-09-11 21:28:20 +0200

    360 fix

--- 360/function.js
+++ 360/function.js
@@ -1,23 +1,32 @@
 $(document).ready(function(){
-  vid = document.getElementById('v0');
-  var input = parseInt($("#seekbar").val());
-  var framenumber = input/13;
-  vid.currentTime = framenumber;
+  //vid = document.getElementById('v0');
+  //var input = parseInt($("#seekbar").val());
+  var framenumber = 1;
+  var last = 41;
+  //vid.currentTime = framenumber;
   //var framenumber = 1;
-  $("#seekbar").on('input', function(){
-    var input = parseInt($("#seekbar").val());
-    console.log(input);
-    var framenumber = input/13;
-    vid.currentTime = framenumber;
-  });
+//  $("#seekbar").on('input', function(){
+    //var input = parseInt($("#seekbar").val());
+    //console.log(input);
+    //var framenumber = input/13;
+  //  vid.currentTime = framenumber;
+  //});
   $("#rechts").click(function(){
-    framenumber++;
-    vid.currentTime = framenumber;
+    if(framenumber < last){
+      framenumber+=1;
+    };
+    var part = $('<div id="part" style="background-image: url(' + framenumber + '.jpg);"></div>');
+    $('#part').replaceWith(part);
     console.log(framenumber);
   });
   $("#links").click(function(){
-    framenumber--;
-    vid.currentTime = framenumber;
+    if(framenumber > 20){
+      framenumber-=1;
+    };
+    var part = $('<div id="part" style="background-image: url(' + framenumber + '.jpg);"></div>');
+    $('#part').replaceWith(part);
     console.log(framenumber);
   });
+
+
 })
