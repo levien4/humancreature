46d2c4eba1a5928d9ef0424078aab73f36c23423
levien4 <levienvdw@gmail.com>
2016-09-22 19:42:31 +0200

    home pagina

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <meta name="theme-color" content="rgb(0,0,47)">
 <title>Home</title>
 <link rel="icon" type="image/x-icon" href="favicon.ico">
+<?php print file_get_contents("http://www.humancreature.nl/scripts.php")?>
 </head>
 <body>
-<?php include_once("analyticstracking.php") ?>
-<?php include 'scripts.php'?>
+<?php print file_get_contents("http://www.humancreature.nl/analyticstracking.php") ?>
 <center>
 <div id="container">
 	<div id="header">
@@ -27,7 +27,29 @@
 
 	<div id="content">
 		<center>
-			<?php include 'menu.php'?>
+			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
+			<div id="home">
+				De mens heeft op allerlei gebied mogelijkheden om in te spelen op eigen levenssituatie of dat van anderen.<br>
+				Die talenten zijn per individu verschillend en soms “onuitputtelijk.”<br>
+				Deze website biedt je inzicht en keuzes om zelf aan de slag te gaan met je scheppende gaven op vooral beeldend gebied.<br>
+				Eye-openers van creatieve talenten op muzikaal, verbaal en ander terrein laat ik aan de andere deskundige over.<br>
+				Alle deze gaven hebben tenslotte een centrale functie binnen de menselijke samenleving = communicatie.
+				Creativiteit is een gave die ieder mens – gelovig of niet – van God heeft gekregen, lees je in Psalm 8
+				<div id="psalm">
+					Heer, onze Heer, groot is uw macht, overal op aarde!<br>
+					In de hemel klinkt een lied over uw macht. Zelfs kleine kinderen zingen over u. <br>
+		      Uw macht is zo groot dat u al uw vijanden verslaat. <br>
+		      Ik kijk naar de hemel die u hebt gemaakt. <br>
+		      Ik kijk naar de maan en de sterren die u daar een plaats hebt gegeven. <br>
+		      En ik denk: Een mens is niet belangrijk, en toch denkt u aan hem. <br>
+		      Een mens is maar klein, en toch vergeet u hem niet. <br>
+		      U hebt de mensen veel macht gegeven, ze zijn bijna zo machtig als goden! <br>
+		      Ze mogen heersen over alles op aarde. U hebt hun alles gegeven: <br>
+					De schapen en de koeien, alle dieren op het land. <br>
+					Ook de vogels in de lucht en de vissen in de zee, alle dieren in het water. <br>
+		       Heer, onze Heer, groot is uw macht, overal op aarde!<br>
+				</div>
+			</div>
 		</center>
 	</div>
 </div>
