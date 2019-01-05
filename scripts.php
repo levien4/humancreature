06e3c67aa186c99ddff45ae21b94f070cf07dd5d
levien4 <levienvdw@gmail.com>
2019-01-05 17:50:34 +0100

    https

--- scripts.php
+++ scripts.php
@@ -2,8 +2,8 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="https://www.humancreature.nl/font.css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<link rel="SHORTCUT ICON" href="http://www.humancreature.nl/favicon.ico" type="image/x-icon" />
-<link rel="ICON" href="http://www.humancreature.nl/favicon.ico" type="image/ico" />
+<link rel="SHORTCUT ICON" href="https://www.humancreature.nl/favicon.ico" type="image/x-icon" />
+<link rel="ICON" href="https://www.humancreature.nl/favicon.ico" type="image/ico" />
 <script type="text/javascript">
 //$(document).ready(function(){
 //$("img").unveil(200);
