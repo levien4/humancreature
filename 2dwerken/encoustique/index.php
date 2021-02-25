4908dc3ef57544ca2d3b461d48d210a43e26ba7c
levien4 <levienvdw@gmail.com>
2021-02-25 19:23:19 +0100

    index fix

--- 2dwerken/encoustique/index.php
+++ 2dwerken/encoustique/index.php
@@ -3,9 +3,10 @@
 <head>
 <title>Encoustique</title>
 <?php print file_get_contents("http://www.humancreature.nl/scripts.php")?>
+<script src="https://www.humancreature.nl/loadpic.js"></script>
 </head>
-<body>
-<?php print file_get_contents("http://www.humancreature.nl/analyticstracking.php") ?>
+<body onload="loadJSON()">
+<?php print file_get_contents("https://www.humancreature.nl/analyticstracking.php") ?>
 <center>
 <div id="container">
 	<div id="header">
@@ -18,7 +19,7 @@
 	<div id="center">
 		<div id="slideshow">
 			<div>
-				<img src="http://www.humancreature.nl/encoustique.jpg"  width="100%">
+				<img src="https://www.humancreature.nl/encoustique.jpg"  width="100%">
 			</div>
 		</div>
 	</div>
@@ -27,20 +28,7 @@
 		<center>
 			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
 			<div id="galerij">
-				<a href="#enc1" id="galerij">
-					<img src="1a.jpeg">
-				</a>
-				<a href="#_" class="lightbox" id="enc1">
-					<img src="1a.jpeg"><br>verwarm de tekenplaat op aluminium - verwarmingselement - <br>en schets met was (-krijt) de afbeelding.
-				</a>
-
-				<a href="#enc2" id="galerij">
-					<img src="21a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="enc2">
-					<img src="21a.jpg">
-					<br>Olieverf op water(spatwerk), encoustique, sjabloondruk, fingerpainting
-				</a>
+			</div>
 		</center>
 	</div>
 </div>
