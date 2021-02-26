f2864dab652da4816c1ebbfa0cc4cf332c02b163
levien4 <levienvdw@gmail.com>
2021-02-26 18:38:26 +0100

    houten modellen toegevoegd

--- 3dwerken/hout/index.php
+++ 3dwerken/hout/index.php
@@ -3,8 +3,9 @@
 <head>
 <title>Hout</title>
 <?php print file_get_contents("http://www.humancreature.nl/scripts.php")?>
+<script src="https://www.humancreature.nl/loadpic.js"></script>
 </head>
-<body>
+<body onload="loadJSON('https://www.humancreature.nl/3dwerken/hout/data.json')">
 <?php print file_get_contents("http://www.humancreature.nl/analyticstracking.php")?>
 <center>
 <div id="container">
@@ -27,40 +28,6 @@
 		<center>
 			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
 			<div id="galerij">
-				<a href="#hout1" id="galerij2">
-					<img src="hout1.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="hout1">
-					<img src="hout1.jpg">
-				</a>
-
-				<a href="#hout2" id="galerij2">
-					<img src="hout2.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="hout2">
-					<img src="hout2.jpg">
-				</a>
-
-				<a href="#hout3" id="galerij2">
-					<img src="hout3.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="hout3">
-					<img src="hout3.jpg">
-				</a>
-
-				<a href="#hout4" id="galerij">
-					<img src="hout4.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="hout4">
-					<img src="hout4.jpg">
-				</a>
-
-				<a href="#hout5" id="galerij">
-					<img src="hout5a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="hout5">
-					<img src="hout5b.JPG">
-				</a>
 			</div>
 		</center>
 	</div>
