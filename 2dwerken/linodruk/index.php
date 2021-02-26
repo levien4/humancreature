ba31c6383a79f0d2bbd0d33853f08ca174eeefe6
levien4 <levienvdw@gmail.com>
2021-02-26 14:37:19 +0100

    linodruk (nog steeds test)

--- 2dwerken/linodruk/index.php
+++ 2dwerken/linodruk/index.php
@@ -3,8 +3,9 @@
 <head>
 <title>Linodruk</title>
 <?php print file_get_contents('http://www.humancreature.nl/scripts.php')?>
+<script src="https://www.humancreature.nl/loadpic.js"></script>
 </head>
-<body>
+<body onload="loadJSON('https://www.humancreature.nl/2dwerken/linodruk/data.json')">
 <?php print file_get_contents("http://www.humancreature.nl/analyticstracking.php")?>
 <center>
 <div id="container">
@@ -18,42 +19,15 @@
 	<div id="center">
 		<div id="slideshow">
 			<div>
-				<img src="http://www.humancreature.nl/encoustique.jpg"  width="100%">
+				<img src="https://www.humancreature.nl/encoustique.jpg"  width="100%">
 			</div>
 		</div>
 	</div>
 
 	<div id="content">
 		<center>
-			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
+			<?php print file_get_contents("https://www.humancreature.nl/menu.php")?>
 			<div id="galerij">
-				<a href="#lino1" id="galerij2">
-					<img src="1b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="lino1">
-					<img src="lin1.JPG">
-				</a>
-		
-				<a href="#lino4" id="galerij2">
-					<img src="4b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="lino4">
-					<img src="lin4.JPG">
-				</a>
-
-				<a href="#lino2" id="galerij">
-					<img src="2b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="lino2">
-					<img src="lin2.JPG">
-				</a>
-
-				<a href="#lino3" id="galerij">
-					<img src="3b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="lino3">
-					<img src="lin3.JPG">
-				</a>
 			</div>
 		</center>
 	</div>
