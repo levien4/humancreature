9146345a1eef1fb600a54609164d51c927d98517
levien4 <levienvdw@gmail.com>
2021-03-01 13:06:32 +0100

    deleted mars

--- marsphotos/function.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var x = 8;
-  for(i = 8; i < 13; i++){
-    var thing = $("<div id='photo' style='background-image:url(" + x +".jpg)'></div>");
-    $("body").append(thing);
-    x += 1;
-  }
-})
