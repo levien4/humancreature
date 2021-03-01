98f509bf5eb3a96164d08b2eca447249418aa50c
levien4 <levienvdw@gmail.com>
2021-03-01 13:20:10 +0100

    fotografie fix??

--- 2dwerken/fotografie/index.php
+++ 2dwerken/fotografie/index.php
@@ -27,77 +27,7 @@
 	<div id="content">
 		<center>
 			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
-			<div id="galerij2">
-				<a href="#img6" id="galerij2">
-					<img src="foto1b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img6">
-					<img src="foto1.JPG">
-					<br>Twee-en-twintigstippelig lieveheersbeestje
-				</a>
-
-				<a href="#img7" id="galerij2">
-					<img src="foto2b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img7">
-					<img src="foto2.jpg">
-					<br>Flamboyant Lady (Zambia)
-				</a>
-
-				<a href="#img8" id="galerij2">
-					<img src="foto3b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img8">
-					<img src="foto3.jpg">
-					<br>Pied Kingfisher (Bontvisvanger) &#9792; (female)
-				</a>
-
-				<a href="#img9" id="galerij2">
-					<img src="foto4b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img9">
-					<img src="foto4.JPG">
-					<br>Rechts Kingfisher &#9794; (male)
-				</a>
-
-				<a href="#img10" id="galerij2">
-					<img src="foto5b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img10">
-					<img src="foto5.JPG">
-					<br>Red Bishop &#9794; (Rooivink)
-				</a>
-
-				<a href="#img11" id="galerij2">
-					<img src="foto6b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img11">
-					<img src="foto6.jpg">
-					<br>Morgenlandschap (Lesotho)
-				</a>
-
-				<a href="#img12" id="galerij2">
-					<img src="foto9b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img12">
-					<img src="foto9.JPG">
-					<br>Vliegenzwam (plaatjeszwam)
-				</a>
-
-				<a href="#img13" id="galerij2">
-					<img src="foto.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img13">
-					<img src="foto8.jpg">
-					<br>Parende Rooivink
-				</a>
-
-				<a href="#img14" id="galerij2">
-					<img src="foto7b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="img14">
-					<img src="foto7.jpg">
-				</a>
+			<div id="galerij">
 			</div>
 		</center>
 	</div>
