b9c4f5ce779629748cb1882fc924a717a36448e5
levien4 <levienvdw@gmail.com>
2021-03-02 20:59:47 +0100

    Keramiek geupdate + schaaltjes toegevoegd

--- 3dwerken/keramiek/index.php
+++ 3dwerken/keramiek/index.php
@@ -3,8 +3,9 @@
 <head>
 <title>Keramiek</title>
 <?php print file_get_contents("http://www.humancreature.nl/scripts.php")?>
+<script src="https://www.humancreature.nl/loadpic.js"></script>
 </head>
-<body>
+<body onload="loadJSON('https://www.humancreature.nl/3dwerken/keramiek/data.json')">
 <?php print file_get_contents("http://www.humancreature.nl/analyticstracking.php")?>
 <center>
 <div id="container">
@@ -27,148 +28,6 @@
 		<center>
 			<?php print file_get_contents("http://www.humancreature.nl/menu.php")?>
 			<div id="galerij2">
-				<a href="#ker1" id="galerij2">
-					<img src="ker1.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker1">
-					<img src="ker1.jpg">
-				</a>
-
-				<a href="#ker2" id="galerij2">
-					<img src="ker2a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker2">
-					<?php print file_get_contents("http://localhost:8080/360")?>
-				</a>
-
-				<a href="#ker3" id="galerij2">
-					<img src="ker3.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker3">
-					<img src="ker3.jpg">
-				</a>
-
-				<a href="#ker4" id="galerij2">
-					<img src="ker4.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker4">
-					<img src="ker4.jpg">
-				</a>
-
-				<a href="#ker6" id="galerij2">
-					<img src="ker6a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker6">
-					<img src="ker6b.jpg">
-				</a>
-
-				<a href="#ker7" id="galerij2">
-					<img src="ker7a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker7">
-					<img src="ker7b.jpg">
-				</a>
-
-				<a href="#ker8" id="galerij2">
-					<img src="ker8b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker8">
-					<img src="ker8.jpg">
-					<br>Onmogelijke giraf
-				</a>
-
-				<a href="#giraffe" id="galerij2">
-					<img src="giraffe.JPG">
-				</a>
-				<a href="#_" class="lightbox" id="giraffe">
-					<img src="giraffe.JPG">
-					<br>Onmogelijke giraf
-				</a>
-
-				<a href="#ker9" id="galerij2">
-					<img src="ker9a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker9">
-					<img src="ker9b.JPG">
-				</a>
-
-				<a href="#ker10" id="galerij2">
-					<img src="ker10.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker10">
-					<img src="ker10.jpg">
-				</a>
-
-				<a href="#ker13" id="galerij2">
-					<img src="ker13b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker13">
-					<img src="ker13a.JPG">
-					<br>Muurdecoratie school voor Buitengewoon Lager Onderwijs te Mariënberg (1993)
-				</a>
-
-				<a href="#ker14" id="galerij2">
-					<img src="ker14b.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker14">
-					<img src="ker14a.JPG">
-					<br>Muurdecoratie school voor Buitengewoon Lager Onderwijs te Mariënberg (1993)
-				</a>
-
-				<a href="#ker15" id="galerij2">
-					<img src="ker15.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker15">
-					<img src="ker15.jpg">
-					<br>Bescherming
-				</a>
-
-				<a href="#ker5" id="galerij">
-					<img src="ker5a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker5">
-					<img src="ker5b.JPG">
-				</a>
-
-				<a href="#ker11" id="galerij">
-					<img src="ker11a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker11">
-					<img src="ker11b.JPG">
-				</a>
-
-				<a href="#ker12" id="galerij">
-					<img src="ker12a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker12">
-					<img src="ker12b.JPG">
-				</a>
-
-				<a href="#ker16" id="galerij">
-					<img src="ker16a.jpg">
-				</a>
-				<a href="#_" class="lightbox" id="ker16">
-					<img src="ker16a.jpg">
-
-				<a href="#ker17" id="galerij2">
-					<img src="ker17.JPG">
-				</a>
-				<a href="#_" class="lightbox" id="ker17">
-					<img src="ker17.JPG">
-
-				<a href="#ker18" id="galerij2">
-					<img src="ker18.JPG">
-				</a>
-				<a href="#_" class="lightbox" id="ker18">
-					<img src="ker18.JPG">
-				</a>
-
-				<a href="#ker19" id="galerij2">
-					<img src="ker19.JPG">
-				</a>
-				<a href="#_" class="lightbox" id="ker19">
-					<img src="ker19.JPG">
-				</a>
 			</div>
 		</center>
 	</div>
